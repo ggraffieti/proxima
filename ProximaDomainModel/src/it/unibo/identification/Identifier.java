91272eb12265701618b1c7017bbd45a24a7b679c
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2018-02-19 18:38:21 +0100

    Domain model started

---
+++ ProximaDomainModel/src/it/unibo/identification/Identifier.java
@@ -0,0 +1,13 @@
+package it.unibo.identification;
+
+public class Identifier {
+	private final String id;
+	
+	public Identifier(final String id) {
+		this.id = id;
+	}
+	
+	public String getIdentifier() {
+		return id;
+	}
+}
