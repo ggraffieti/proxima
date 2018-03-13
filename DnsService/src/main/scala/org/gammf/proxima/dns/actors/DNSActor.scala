bb4532d48bc1fb722a92c558adc00dde63f7bcf7
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2018-03-13 23:49:30 +0100

    DNS: definition of a basic trait representing an actor into the dns hierarchy

---
+++ DnsService/src/main/scala/org/gammf/proxima/dns/actors/DNSActor.scala
@@ -0,0 +1,27 @@
+package org.gammf.proxima.dns.actors
+
+import akka.actor.Actor
+import org.gammf.proxima.dns.util.Role._
+import org.gammf.proxima.dns.util.Service.StringService
+
+/**
+  * Represents a simple actor of the DNS hierarchy. It's characterized by a name, a role into the DNS system and a service.
+  */
+trait DNSActor extends Actor{
+  /**
+    * Returns the name of this node.
+    */
+  def name: String
+
+  /**
+    * Returns the role of this node.
+    */
+  def role: Role
+
+  /**
+    * Returns the service offered by this node.
+    */
+  def service: StringService
+}
+
+
