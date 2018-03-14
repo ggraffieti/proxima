a4084d621ea926d3dbfd7b40b52254065c80c1d0
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2018-03-14 10:05:51 +0100

    DNS: documentation in role

--- DnsService/src/main/scala/org/gammf/proxima/dns/util/Role.scala
+++ DnsService/src/main/scala/org/gammf/proxima/dns/util/Role.scala
@@ -6,8 +6,21 @@ package org.gammf.proxima.dns.util
 object Role extends Enumeration {
   type Role = Value
 
-  val Root,
+  val
+  /**
+    * Represents the root of the Domain Name System hierarchy.
+    * It can't have a parent node. It can have both [[InternalNode]] and [[LeafNode]] as children.
+    */
+  Root,
+  /**
+    * Represents an internal node of the Domain Name System hierarchy.
+    * It can have both [[Root]] and [[InternalNode]] as parent. It can have both [[InternalNode]] and [[LeafNode]] as children.
+    */
   InternalNode,
+  /**
+    * Represents a leaf node of the Domain Name System hierarchy.
+    * It can have both [[Root]] and [[InternalNode]] as parent. It can't have children.
+    */
   LeafNode
   = Value
 }
