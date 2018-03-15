825642c7df79b7b440b093e9cee128980ab6cc09
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2018-03-15 21:10:07 +0100

    DNS: implicit conversions about address creation messages added

--- DnsService/src/main/scala/org/gammf/proxima/dns/general/actors/package.scala
+++ DnsService/src/main/scala/org/gammf/proxima/dns/general/actors/package.scala
@@ -1,8 +1,9 @@
 package org.gammf.proxima.dns.general
 
-import org.gammf.proxima.dns.general.messages.{ExternalAddressRequestMessage, ExternalAddressResponseOKMessage}
+import org.gammf.proxima.dns.general.messages._
 import org.gammf.proxima.dns.hierarchy.messages.{AddressRequestMessage, AddressResponseOKMessage}
 import org.gammf.proxima.dns.utils.Service
+
 import language.implicitConversions
 import language.reflectiveCalls
 
@@ -14,4 +15,11 @@ package object actors {
 
   implicit def addressResponse2ExternalAddressResponse(msg: AddressResponseOKMessage): ExternalAddressResponseOKMessage =
     ExternalAddressResponseOKMessage(ipAddress = msg.address.ip, port = msg.address.port)
+
+  implicit def externalAddressCreationRequest2AddressCreationRequest(msg: ExternalAddressCreationRequestMessage): AddressCreationRequestMessage =
+    AddressCreationRequestMessage(service = msg.service.split(SPLIT_CHAR).map(Service(_)).fold(Service())((a, b) => a ++ b),
+      ipAddress = msg.ipAddress, port = msg.port)
+
+  implicit def addressCreationResponse2ExternalAddressCreationResponse(msg: AddressCreationResponseMessage): ExternalAddressCreationResponseMessage =
+    ExternalAddressCreationResponseMessage(result = msg.result)
 }
