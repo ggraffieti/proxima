7a181b2303f9a186b597daa689e49ca19cbdb683
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2018-03-18 21:00:53 +0100

    edit authorization checker, now it use public key infrastructure

--- FirstAidService/src/digitalSignature/authorizationChecker.ts
+++ FirstAidService/src/digitalSignature/authorizationChecker.ts
@@ -1,5 +1,27 @@
+import * as crypto from "crypto";
+import { PublicKeyQueries } from "./publicKeyQueries";
+
 export class AuthorizationChecker {
+  
   public static verifyDigitalSignature(operatorID: string, targetID: string, digitalSignature:string ): Promise<boolean> {
-    return Promise.resolve(true); // mock object, always return true for now.
-  }  
+
+    return new Promise((resolve, reject) => {
+      PublicKeyQueries.getPublicKey(operatorID).then((key) => {
+        let verifier = crypto.createVerify('sha256');
+        verifier.update(targetID);
+        let verification =  verifier.verify(key, digitalSignature, "base64");
+        if (verification) {
+          resolve(true);
+        }
+        else {
+          reject();
+        }
+      }).catch((err) => reject(err));
+    })
+    
+
+    //return Promise.resolve(true); // mock object, always return true for now.
+  }
+
+  
 }
