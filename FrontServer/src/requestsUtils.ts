8c5f1cc82120ce127c13daf78ced3adbfa12c622
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2018-03-26 20:37:31 +0200

    Front server: little change in the JSON format of the services addresses file

--- FrontServer/src/requestsUtils.ts
+++ FrontServer/src/requestsUtils.ts
@@ -1,14 +1,33 @@
 import * as fs from "fs";
 
 /**
+ * Represents the address of a component, composed of IP address and port number.
+ */
+interface Address {
+    ip: string;
+    port: number;
+}
+
+/**
  * Represents a Proxima web service. Contains all the information needed in order to contact the service.
  * A service is described by its name, its address (IP address and port number) and the path it listens to.
  */
 interface Service {
-    name: String;
-    ip: String;
-    port: Number;
-    path: String;
+    name: string;
+    address: Address;
+    path: string;
+}
+
+/**
+ * Represents all the information needed in order to make a HTTP(S) request to a Proxima web service.
+ * Contains the HTTP method, the component URI, a numeric value indicating the timeout of the request and a boolean value
+ * (true if the whole response will be analyzed, false if only the response body will matter).
+ */
+interface RequestInfo {
+    method: string;
+    uri: string;
+    timeout: number;
+    resolveWithFullResponse: boolean
 }
 
 /**
@@ -44,16 +63,15 @@ export class RequestsCreator {
     /**
      * This method generates a request to the Proxima users authorization service, asking if the subject, identified by a
      * targetID, allowed data access for the given service.
-     * @param {String} targetID the identifier of the target user.
-     * @param {String} service the indication of the service in question.
-     * @returns {{method: string; uri: string; timeout: number; resolveWithFullResponse: boolean}} the request to the
-     * Proxima users authorization service.
+     * @param {string} targetID the identifier of the target user.
+     * @param {string} service the indication of the service in question.
+     * @returns {RequestInfo} the request to the Proxima users authorization service.
      */
-    getUserAuthRequest(targetID: String, service: String) {
+    getUserAuthRequest(targetID: string, service: string): RequestInfo {
         let srv = this.services.find(s => s.name === "usersAuthorizationsService");
         return {
             method: 'GET',
-            uri: "http://" + srv.ip + ":" + srv.port + "/" + srv.path + "?targetID=" + targetID + "&service=" + service,
+            uri: "http://" + srv.address.ip + ":" + srv.address.port + "/" + srv.path + "?targetID=" + targetID + "&service=" + service,
             timeout: RequestsCreator.REQUEST_TIMEOUT,
             resolveWithFullResponse: true
         };
@@ -62,15 +80,14 @@ export class RequestsCreator {
     /**
      * This method generates a request to the Proxima DNS service, asking for the address of a component offering the
      * specified service.
-     * @param {String} service the indication of the service.
-     * @returns {{method: string; uri: string; timeout: number; resolveWithFullResponse: boolean}} the request to the
-     * Proxima DNS service.
+     * @param {string} service the indication of the service.
+     * @returns {RequestInfo} the request to the Proxima DNS service.
      */
-    getDNSRequest(service: String) {
+    getDNSRequest(service: string): RequestInfo {
         let srv = this.services.filter(s => s.name === "dnsService")[0];
         return {
             method: 'GET',
-            uri: "http://" + srv.ip + ":" + srv.port + "/" + srv.path + "/" + service,
+            uri: "http://" + srv.address.ip + ":" + srv.address.port + "/" + srv.path + "/" + service,
             timeout: RequestsCreator.REQUEST_TIMEOUT,
             resolveWithFullResponse: true
         };
@@ -80,14 +97,13 @@ export class RequestsCreator {
      * This method generates a request to a Proxima component, requesting the data of the subject, identified by its
      * targetID. The data could be protected, so the operator that wants to access them have to be authorized, through
      * the indication of its operatorID and its personal signature.
-     * @param {{ip: String; port: Number}} address the component address, composed of IP address and port number.
-     * @param {String} targetID the identifier of the target user.
-     * @param {String} operatorID the identifier of the operator.
-     * @param {String} signature the signature of the operator.
-     * @returns {{method: string; uri: string; timeout: number; resolveWithFullResponse: boolean}} the request to the
-     * specified component.
+     * @param {Address} address the component address, composed of IP address and port number.
+     * @param {string} targetID the identifier of the target user.
+     * @param {string} operatorID the identifier of the operator.
+     * @param {string} signature the signature of the operator.
+     * @returns {RequestInfo} the request to the specified component.
      */
-    getDataRequest(address: { ip: String, port: Number}, targetID: String, operatorID: String, signature: String) {
+    getDataRequest(address: Address, targetID: string, operatorID: string, signature: string): RequestInfo {
         let srv = this.services.filter(s => s.name === "dataService")[0];
         return {
             method: 'GET',
