24f6480efd124fe9f2ddf32a8864113c4beafae4
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2018-03-31 13:05:14 +0200

    make IMedicalData properties read only + documentation

--- FirstAidService/src/main/medicalData/utils/medicalData.ts
+++ FirstAidService/src/main/medicalData/utils/medicalData.ts
@@ -1,5 +1,8 @@
 import { IMedicalData } from "./IMedicalData";
 
+/**
+ * A simple implementation of the interface IMedicalData.
+ */
 export class MedicalData implements IMedicalData {
   public constructor(public name: string, public surname: string, public CF: string, public birthDate: Date, public bloodGroup: string, public organDonor: string, public medicalConditions: string[] = [], public drugAllergies: string[] = [], public otherAllergies: string[] = [], public medications: string[] = []) { }
 }
