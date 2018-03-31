9fe281c264765cfd1c3e7b2c830098710a4b5b22
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2018-03-31 12:52:19 +0200

    documentation

--- FirstAidService/src/main/logger/loggerFactory.ts
+++ FirstAidService/src/main/logger/loggerFactory.ts
@@ -2,12 +2,27 @@ import { ILogger } from "./ILogger";
 import { RemoteLogger } from "./remoteLogger";
 import { LocalFileLogger } from "./localFileLogger";
 
+/**
+ * A factory class that builds loggers.
+ * 
+ * This class is static, it doesn't have state and it cannot be istantiate.
+ */
 export class LoggerFactory {
 
+  private constructor() {}
+
+  /**
+   * Returns a new instance of a LocalFileLogger, that logs on the given files.
+   * @param logAccesFilePath Path to the file where authorized data access will be logged.
+   * @param logDenyFilePath Path to the file where unauthorized data access will be logged.
+   */
   public static localFileLogger(logAccesFilePath: string, logDenyFilePath: string): ILogger {
     return new LocalFileLogger(logAccesFilePath, logDenyFilePath); 
   }
 
+  /**
+   * Returns a new instance of a RemoteLogger, that logs on a remote machine.
+   */
   public static remoteLogger(): ILogger {
     return new RemoteLogger();
   }
