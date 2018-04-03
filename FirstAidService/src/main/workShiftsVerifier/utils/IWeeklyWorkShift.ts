72c7d80f2e5da1a574f259f7114512317d8044f0
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2018-04-03 11:18:10 +0200

    fix error in documentation

--- FirstAidService/src/main/workShiftsVerifier/utils/IWeeklyWorkShift.ts
+++ FirstAidService/src/main/workShiftsVerifier/utils/IWeeklyWorkShift.ts
@@ -1,9 +1,13 @@
 export interface IWeeklyWorkShift {
-    getRescuer(): string;
-    /**
-     * Checks if this worker has a shift in the given day of the week, and in the given time.
-     * @param dayOfWeek The day of the week (0-6) 0 is monday, 6 is sunday.
-     * @param time the time of the day, in minutes after midnight (e.g. 5:24 AM = 324 (60*5+24)). 
-     */
-    checkShift(dayOfWeek: number, time: number): boolean;
+
+  /**
+   * Returns the rescuer asociated with this work shift.
+   */
+  getRescuer(): string;
+  /**
+   * Checks if this worker has a work shift in the given day of the week, and in the given time.
+   * @param dayOfWeek The day of the week (1-7) 1 is monday, 7 is sunday.
+   * @param time the time of the day, in minutes after midnight (e.g. 5:24 AM = 324 (60*5+24)). 
+   */
+  checkShift(dayOfWeek: number, time: number): boolean;
 }
