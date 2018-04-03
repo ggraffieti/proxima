26d0a32db6c65665147dce55bf51efcbf25a6b09
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2018-04-03 11:11:47 +0200

    documentation

--- FirstAidService/src/main/network/networkManager.ts
+++ FirstAidService/src/main/network/networkManager.ts
@@ -1,13 +1,27 @@
 import * as request from "request";
 
+/**
+ * A static class that manages the network, sending HTTP requests.
+ */
 export class NetworkManager {
 
   private static NETWORK_TIMEOUT = 1000; // 1 second
 
+  /**
+   * Send an HTTP GET request.
+   * @param url the URL of the request
+   * @param callback a callback attached to the request.
+   */
   public static sendHttpGet(url: string, callback: (error, response, body) => void) {
     request.get(url, {timeout: NetworkManager.NETWORK_TIMEOUT}, callback);
   }
 
+  /**
+   * Send an HTTP POST request.
+   * @param url the URL of the request.
+   * @param data optional data, attached to the request, in JSON form.
+   * @param callback a callback attached to the request.
+   */
   public static sendHttpPost(url: string, data: object, callback: (error, response, body) => void) {
     request.post(url, {timeout: NetworkManager.NETWORK_TIMEOUT, body: data, json: true}, callback);
   }
