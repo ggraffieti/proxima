80b7f15d39982dda0ff51c8895c0d4e44c2da1e3
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2018-05-01 11:40:11 +0200

    fix bug in digital signature verification test

--- FirstAidService/src/test/digitalSignature/digitalSignature.test.ts
+++ FirstAidService/src/test/digitalSignature/digitalSignature.test.ts
@@ -7,33 +7,36 @@ import { AuthorizationChecker } from "../../main/digitalSignature/authorizationC
 describe('Authorization checker', () => {
 
   it('should correctly verify a digital signature', () => {
-    const data = "abcdefg";
+    const patientID = "abcdefg";
+    const operatorID = "rescuer1";
     const signer = crypto.createSign("sha256");
-    signer.update(data);
-    const privateKey = fs.readFileSync("test/resources/correct_private.pem", "utf8");
+    signer.update(patientID + operatorID);
+    const privateKey = fs.readFileSync("src/test/resources/correct_private.pem", "utf8");
     const digitalSignature = signer.sign(privateKey, "base64");
-    return AuthorizationChecker.verifyDigitalSignature("123456789", data, digitalSignature)
+    return AuthorizationChecker.verifyDigitalSignature(operatorID, patientID, digitalSignature)
       .then((res)=> expect(res).to.equal(true));
   });
 
   it('should reject authentication if rescuer is not present', () => {
-    const data = "qwerty";
+    const patientID = "abcdefg";
+    const operatorID = "aFakeRescuer";
     const signer = crypto.createSign("sha256");
-    signer.update(data);
-    const privateKey = fs.readFileSync("test/resources/correct_private.pem", "utf8");
+    signer.update(patientID + operatorID);
+    const privateKey = fs.readFileSync("src/test/resources/correct_private.pem", "utf8");
     const digitalSignature = signer.sign(privateKey, "base64");
-    return AuthorizationChecker.verifyDigitalSignature("aFakeRescuer", data, digitalSignature)
+    return AuthorizationChecker.verifyDigitalSignature(operatorID, patientID, digitalSignature)
       .then((_)=> {throw new Error('Expected method to reject.')})
       .catch((_) => expect(_));
   });
 
   it('should reject authentication if digital signature is wrong', () => {
-    const data = "qwerty";
+    const patientID = "abcdefg";
+    const operatorID = "rescuer1";
     const signer = crypto.createSign("sha256");
-    signer.update(data);
-    const privateKey = fs.readFileSync("test/resources/fake_private.pem", "utf8");
+    signer.update(patientID + operatorID);
+    const privateKey = fs.readFileSync("src/test/resources/fake_private.pem", "utf8");
     const digitalSignature = signer.sign(privateKey, "base64");
-    return AuthorizationChecker.verifyDigitalSignature("123456789", data, digitalSignature)
+    return AuthorizationChecker.verifyDigitalSignature(operatorID, patientID, digitalSignature)
       .then((_)=> {throw new Error('Expected method to reject.')})
       .catch((_) => expect(_));
   });
