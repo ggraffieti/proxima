1903907dd18ecf3b968fdde2f668a9fe31466ff9
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2018-05-03 22:18:35 +0200

    Logger : request handler -> documentation

--- Logger/src/handler/handleRequest.ts
+++ Logger/src/handler/handleRequest.ts
@@ -4,18 +4,22 @@ import { Logger } from "../logger/logger";
 
 export class HandleRequest {
 
+  /**
+   Method that handle the request of a logging for correctly accessed data.
+   */
   public static handleAccessLogRequest(req: Request, res: Response) {
-    console.log(req.body.rescuerID);
-    console.log(req.body.patientID);
     Logger.getInstance().logDataAccess(req.body.rescuerID, req.body.patientID)
       .then((_) => res.send())
       .catch((_) => res.status(500).send());
-  } 
+  }
 
+  /**
+   Method that handle the request of a logging for denied data access.
+   */
   public static handleDeniedLogRequest(req: Request, res: Response) {
     Logger.getInstance().logDataAccessDenied(req.body.rescuerID, req.body.patientID)
       .then((_) => res.send())
       .catch((_) => res.status(500).send());
-  } 
+  }
 
 }
